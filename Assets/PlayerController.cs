e888bafb2767c9ccd4b6384c8f06e1bc92c86113
Filip Jerga <jerga99@gmail.com>
2021-01-05 12:09:58 +0100

    Update lifecycle functions

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,53 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Recap
-    // Awake - One approach is to init an objects own references and variables
-    // Start - use or create rederences to other objects and their components
-
-
-    // is called only once
-    // is called when script is disabled
-    // is called before the start
-    // is called when other objects are initialized
-    void Awake()
+    // is called on consistent frame rate
+    // manipulate physics
+    // for example: to manipulate "rigidbody" component
+    void FixedUpdate()
     {
-        Debug.Log("Calling Awake");
+        Debug.Log(Time.fixedDeltaTime);
     }
 
-    // Start is called before the first frame update
-    // is called when script is enabled
-    // can be used as coroutine (execution of start can be delayed)
-    void Start()
+    // Update is called once per frame
+    // is not consistent
+    // one frame can take longer than other
+    // you can use update:
+    // to receive input from the user
+    // do operation with timers
+    // manipulate non physics objects
+    void Update()
     {
-        Debug.Log("Calling Start");
     }
 
-    // is Called when script is enabled
-    void OnEnable()
+    // is called after update
+    // for example: manipulate camera
+    void LateUpdate()
     {
-        Debug.Log("Calling OnEnable");
     }
-
-    void OnDisable()
-    {
-        Debug.Log("Calling OnDisable");
-    }
-
-    //// Update is called once per frame
-    //void FixedUpdate()
-    //{
-    //    Debug.Log("Calling Fixed Update");
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    Debug.Log("Calling Update");
-    //}
-
-    //void LateUpdate()
-    //{
-    //    Debug.Log("Calling Late Update");
-    //}
 }
