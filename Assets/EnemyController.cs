a1a742ed9cd3850954e81a583cd5dd6a80ed79e8
Filip Jerga <jerga99@gmail.com>
2021-01-06 10:46:28 +0100

    Get reference to own component

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
-
+    // Start is called before the first frame update
     void Awake()
     {
         Stats stats = GetComponent<Stats>();
         Debug.Log(stats.health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
