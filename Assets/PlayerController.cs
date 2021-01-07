f96ffa4139dc471d629e069125bc2d7e3924b508
Filip Jerga <jerga99@gmail.com>
2021-01-07 13:32:43 +0100

    Restrict movement of the player

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,10 +6,27 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
 
+    public Transform leftWall;
+    public Transform rightWall;
+
     // if fps is 150 then update is called 150 and position is changes by 150
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
+
+        Debug.Log("PLayer Z position: " + transform.position.x);
+        Debug.Log("Left Wall Z position: " + leftWall.position.x);
+
+        if (transform.position.x <= leftWall.position.x)
+        {
+            return;
+        }
+
+        if (transform.position.x >= rightWall.position.x)
+        {
+            return;
+        }
+
 
         transform.position = new Vector3(
             transform.position.x + horizontalInput * speed * Time.deltaTime,
