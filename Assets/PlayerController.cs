40f130f0ed085ccb384c1827a1ea04e9b8a95bd4
Filip Jerga <jerga99@gmail.com>
2021-01-07 12:17:34 +0100

    Fix movement speed

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
 
-
+    // if fps is 150 then update is called 150 and position is changes by 150
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         transform.position = new Vector3(
-            transform.position.x + horizontalInput,
+            transform.position.x + horizontalInput * speed * Time.deltaTime,
             1,
-            transform.position.z + verticalInput);
+            transform.position.z + verticalInput * speed * Time.deltaTime);
     }
 }
