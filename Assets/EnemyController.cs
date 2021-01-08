d3634b508eccfe5f39e4f94c51127a2776c49a69
Filip Jerga <jerga99@gmail.com>
2021-01-08 10:56:21 +0100

    Enemy movement

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -4,4 +4,13 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public float speed;
+
+    private void Update()
+    {
+        transform.position = new Vector3(
+            transform.position.x,
+            transform.position.y,
+            transform.position.z - speed * Time.deltaTime);
+    }
 }
