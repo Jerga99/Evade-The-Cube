af2b9196a9bf4e7cad92f938d02e6222b33cfba6
Filip Jerga <jerga99@gmail.com>
2021-01-11 09:27:13 +0100

    Create catch enemy from script

--- Assets/SpawnManager.cs
+++ Assets/SpawnManager.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject catcherPrefab;
+
     public Vector2 spawnRangeX;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnEnemy", 0, 2.0f);
+        InvokeRepeating(nameof(SpawnEnemy), 0, 2.0f);
+        InvokeRepeating(nameof(SpawnCatcher), 1.0f, 2.0f);
+    }
+
+    private void SpawnCatcher()
+    {
+        Vector3 spawnPosition = new Vector3(
+            Random.Range(spawnRangeX[0], spawnRangeX[1]),
+            catcherPrefab.transform.position.y,
+            catcherPrefab.transform.position.z);
+
+        Instantiate(
+            catcherPrefab,
+            spawnPosition,
+            catcherPrefab.transform.rotation);
     }
 
     private void SpawnEnemy()
